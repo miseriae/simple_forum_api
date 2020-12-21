41dfbaa306db425039649c51f1a9a287ca934343
miseriae <zuyev.frm@gmail.com>
2020-12-21 17:06:54 +0200

    Add docker build

---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+python manage.py makemigrations
+python manage.py migrate
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
+exec "$@"
